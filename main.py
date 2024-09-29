6320eabf34d4fd145e5f9b778efa433979c28427
billytimmy666 <100006906+billytimmy666@users.noreply.github.com>
2024-09-28 20:30:02 -0400

    initial commit

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from flasgger import swag_from
 
 main = Blueprint('main', __name__)
 
+
 @main.route('/split_flac', methods=['GET'])
 @swag_from({
     'responses': {
@@ -37,7 +38,6 @@ def run_split_script():
     }
 })
 def run_mp3_script():
-    print("foo", os.path.abspath(os.path.curdir))
     try:
         result = subprocess.run(['python', "-u", 'app/lossless2mp3.py'], capture_output=True, text=True)
         return f"Script output:\n{result.stdout}\n\n{result.stderr}"
@@ -47,6 +47,32 @@ def run_mp3_script():
 
 @main.route('/video_metadata_to_location', methods=['GET'])
 @swag_from({
+    'parameters': [
+        {
+            'name': 'input_string',
+            'in': 'query',
+            'type': 'string',
+            'required': True,
+            'description': 'The input string for processing'
+        },
+        {
+            'name': 'remove_subs',
+            'in': 'query',
+            'type': 'boolean',
+            'default': True,
+            'required': True,
+            'description': 'Whether to include metadata (checkbox)'
+        },
+        {
+            'name': 'only_english_subs',
+            'in': 'query',
+            'type': 'boolean',
+            'required': True,
+            'default': True,
+            'description': 'Whether to include metadata (checkbox)'
+        }
+
+    ],
     'responses': {
         200: {
             'description': 'Script output',
@@ -68,14 +94,14 @@ def run_video_metadata_to_location():
 
     # Get the 'include_metadata' boolean parameter (default to False if not provided)
     remove_subs = request.args.get('remove_subs', 'true').lower() in ['true', '1', 'yes']
-    only_englsh_subs = request.args.get('only_englsh_subs', 'true').lower() in ['true', '1', 'yes']
+    only_english_subs = request.args.get('only_english_subs', 'true').lower() in ['true', '1', 'yes']
 
     if not input_string:
         return "Missing 'input_string' parameter.", 400
 
     try:
         # Prepare the command for subprocess with optional include_metadata flag
-        command = ['python', '-u', 'app/video_metadata_to_location.py', input_string, remove_subs, only_englsh_subs]
+        command = ['python', '-u', 'app/video_metadata_to_location.py', input_string, remove_subs, only_english_subs]
 
         # Run the script with the input string and optional flag
         result = subprocess.run(command, capture_output=True, text=True)
