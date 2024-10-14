07059d466012fe85c3cfffda51c11debea3c9b35
billytimmy666 <100006906+billytimmy666@users.noreply.github.com>
2024-10-14 14:55:33 -0400

    initial commit

--- app/settings.py
+++ app/settings.py
@@ -2,7 +2,7 @@ import os
 
 from pydantic_settings import BaseSettings
 from pydantic import validator
-from constants import DB_FILENAME
+from constants import DB_FILENAME, ROOT_DIR
 
 
 class Settings(BaseSettings):
@@ -49,8 +49,8 @@ class Settings(BaseSettings):
         if os.environ.get('LOCAL') == 'true':
             env_file = '.env-local'
         else:
-            env_file = '.env-docker'
-
+            env_file = os.path.join(ROOT_DIR, 'app', '.env-docker')
+        print(env_file, os.path.exists(env_file))
     @validator('DATABASE_FILE', pre=True, always=True)
     def set_database_full_path(cls, value, values):
         return f"{values.get('DB_DIR')}/{DB_FILENAME}"
