49ecc527e43d84a25b1f96326162182e8470ee1c
ad-repo <100006906+ad-repo@users.noreply.github.com>
2025-09-20 10:24:06 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -10,4 +10,9 @@ def create_app():
     from main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
+    # Redirect / → /apidocs
+    @app.route('/')
+    def index():
+        return redirect('/apidocs')
+
     return app
